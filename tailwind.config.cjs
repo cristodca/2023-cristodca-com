3e2fd7ac2b823032ee95c04d9e5fbce6e1b24ebf
@cristodca (Cristopher D. Chavez) <cristodca.studio@gmail.com>
2023-01-24 16:23:43 -0600

    navbar, hero and about me section

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+      colors: {
+        'primary': '#657ED4',
+        'secondary': '#9452C8',
+        'danger': '#FF5B48',
+        'alert': '#D0B72B',
+        'light': '#fafafa',
+        'dark': '#272727',
+      }
+    },
+	},
+	plugins: [],
+}
